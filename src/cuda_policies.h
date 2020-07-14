1757b13265a47d15779e756233a3937c5b322a04
Will Killian <william.killian@millersville.edu>
2020-07-13 21:23:27 -0400

    Updated policies; fixed makefile

--- src/cuda_policies.h
+++ src/cuda_policies.h
@@ -174,15 +174,14 @@ using XRHS_POL =
 // WKK -- important
 using XRHS_POL_ASYNC =
   RAJA::KernelPolicy<
-    RAJA::statement::CudaKernelAsync<
-      // tiles match the ranges
+    RAJA::statement::CudaKernelFixedAsync<64,
       RAJA::statement::Tile<2, RAJA::tile_fixed<4>, RAJA::cuda_block_z_loop,
         RAJA::statement::Tile<1, RAJA::tile_fixed<4>, RAJA::cuda_block_y_loop,
-          RAJA::statement::Tile<0, RAJA::tile_fixed<16>, RAJA::cuda_block_x_loop,
+          RAJA::statement::Tile<0, RAJA::tile_fixed<4>, RAJA::cuda_block_x_loop,
             RAJA::statement::For<2, RAJA::cuda_thread_z_direct,
-             RAJA::statement::For<1, RAJA::cuda_thread_y_direct,
-              RAJA::statement::For<0, RAJA::cuda_thread_x_direct,
-                RAJA::statement::Lambda<0>>>>>>>>>;
+              RAJA::statement::For<1, RAJA::cuda_thread_y_direct,
+                RAJA::statement::For<0, RAJA::cuda_thread_x_direct,
+                  RAJA::statement::Lambda<0>>>>>>>>>;
 
 using TWILIGHTSG_POL =
     RAJA::KernelPolicy<RAJA::statement::CudaKernel<RAJA::statement::Tile<
